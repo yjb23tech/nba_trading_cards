fc58b1d498825077a566c34a53532b86b272ef9d
yjb23tech <yjb23tech@gmail.com>
2023-12-27 15:19:22 +0000

    This is clean

--- components/NaviBar.jsx
+++ components/NaviBar.jsx
@@ -1,11 +1,11 @@
-import basketball_logo_white from "../public/images/"
+import basketball_logo_white from "../public/images/basketball_logo_white.png"
 
 const NaviBar = () => {
     return (
         <nav className="universal-navi">
             <h3>Starting</h3>
-            <span><img src={basketball_logo_white}/></span>
-            <span><h3>Five</h3></span>
+            <img src={basketball_logo_white}/>
+            <h3>Five</h3>
         </nav>
     )
 }
