26aa583a5828c6d268e6459e88bb18fdcf212725
yjb23tech <yjb23tech@gmail.com>
2023-12-27 15:12:01 +0000

    Hypocrisy

--- components/NaviBar.jsx
+++ components/NaviBar.jsx
@@ -1,7 +1,11 @@
+import basketball_logo_white from "../public/images/"
+
 const NaviBar = () => {
     return (
         <nav className="universal-navi">
-            <h3>Starting Five</h3>
+            <h3>Starting</h3>
+            <span><img src={basketball_logo_white}/></span>
+            <span><h3>Five</h3></span>
         </nav>
     )
 }
